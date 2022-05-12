e92d16c4f21c0de05edf2670ce883839f873a4f0
artur.sniegowski <username@users.noreply.github.com>
2022-05-12 15:39:20 +0200

    Update

--- learning_blog/learning_blog_app/admin.py
+++ learning_blog/learning_blog_app/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from learning_blog_app.models import Topic, Entry
+from learning_blog.learning_blog_app.models import Topic, Entry
 
 admin.site.register(Topic)
 admin.site.register(Entry)
